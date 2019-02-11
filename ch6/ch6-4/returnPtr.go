5a90b28a003179dda3ce0ce7e1001f4446c3533a
JayJay <youngtip@gmail.com>
2019-02-11 10:09:56 +0900

    .

---
+++ ch6/ch6-4/returnPtr.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func returnPtr(x int) *int {
+	y := x * x
+	return &y
+}
+
+func main() {
+	sq := returnPtr(10)
+	fmt.Println("sq: ", *sq)
+
+	fmt.Println("sq: ", sq)
+}
